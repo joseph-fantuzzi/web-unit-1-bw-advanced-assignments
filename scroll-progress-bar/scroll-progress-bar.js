da4cf37ca972b8be4b3be94523c6c79de59c28db
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-02-02 18:28:44 -0500

    completed the scroll progress bar project

--- scroll-progress-bar/scroll-progress-bar.js
+++ scroll-progress-bar/scroll-progress-bar.js
@@ -1,3 +1,14 @@
 // most of the work here will be done with logic and a little math
 // what's the usual measurement of a progress bar? you'll need to measure the same way here
 // use that measurement to update the style of your inner fill element as you scroll
+
+const scrollProgress = document.querySelector(".scroll-progress");
+
+document.addEventListener("scroll", () => {
+  const totalHeightOfPage = document.body.scrollHeight;
+  const currentDistanceFromTop = document.documentElement.scrollTop;
+  const windowHeight = document.documentElement.clientHeight;
+  const scrolledPercentage = (currentDistanceFromTop / (totalHeightOfPage - windowHeight)) * 100;
+
+  scrollProgress.style.width = Math.round(scrolledPercentage) + "%";
+});

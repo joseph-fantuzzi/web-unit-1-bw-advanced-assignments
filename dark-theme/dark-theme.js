f1883b26f2c5280c59043f206a066a479e2d5ce6
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-02-02 14:03:57 -0500

    completed the dark mode project

--- dark-theme/dark-theme.js
+++ dark-theme/dark-theme.js
@@ -1,1 +1,13 @@
 // you need to create DOM selectors to grab the correct HTML elements and update their styles to the opposite theme's css classes when you want to toggle the theme off and on
+
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+const div = document.querySelector(".btn-container");
+const btnDiv = document.querySelector("button div");
+
+button.addEventListener("click", () => {
+  body.classList.toggle("active-body");
+  div.classList.toggle("active-div");
+  button.classList.toggle("active-btn");
+  btnDiv.classList.toggle("active-btn-div");
+});

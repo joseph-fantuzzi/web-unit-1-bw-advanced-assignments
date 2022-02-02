3b2c33c067db0838b24e457b2c96d778ec0cb973
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-02-02 17:04:54 -0500

    completed the modal project

--- modal/modal.js
+++ modal/modal.js
@@ -1,1 +1,13 @@
 // you will need to toggle the visibility off and on with DOM selectors
+
+const homeButton = document.querySelector(".home-btn");
+const modalContainer = document.querySelector(".modal-container");
+const closeBtn = document.querySelector(".close-btn");
+
+homeButton.addEventListener("click", () => {
+  modalContainer.classList.add("modal-container-active");
+});
+
+closeBtn.addEventListener("click", () => {
+  modalContainer.classList.remove("modal-container-active");
+});
